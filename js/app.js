45750083a0a82979a470a34e981571a38566cac7
GBC2015 <minecraftdiamond415@gmail.com>
2017-10-23 20:10:52 +0400

    Revert "Revert "edited app.ja""
    This reverts commit 8fdcf367d209b1076b40e963b85732309b512d13.

--- js/app.js
+++ js/app.js
@@ -11,6 +11,11 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 /* Otherwise just put the config content (json): */
 
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
 particlesJS('particles-js',
   
   {
